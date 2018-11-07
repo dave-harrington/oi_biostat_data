9cf64131a243cdcc3f41aab12b177cd7d797e73b
Julie Vu <julievu777@gmail.com>
2018-11-07 14:31:55 -0500

    adding thermometry dataset

---
+++ data-raw/thermometry.R
@@ -0,0 +1,14 @@
+library(devtools)
+
+thermometry = read.csv("thermometry.csv")
+
+colnames(thermometry) <- c("body.temp", "gender", "heart.rate")
+
+thermometry$gender <- factor(thermometry$gender,
+                             levels = c(1, 2),
+                             labels = c("female", "male"))
+
+
+devtools::use_data(thermometry, overwrite = TRUE)
+
+save(thermometry, file = "thermometry.Rdata")
