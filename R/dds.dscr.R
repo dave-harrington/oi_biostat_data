cb5730a51c2fecd4a59461573d1b8bd3b78e2115
OI-Biostat <38590928+dave-harrington@users.noreply.github.com>
2024-03-15 14:12:10 -0400

    dds.discr now installed.  This push has files we do want in the final build, such as the data-raw files.

---
+++ R/dds.dscr.R
@@ -0,0 +1,35 @@
+#' A dataframe of 1000 rows and 6 variables used to study the association
+#'   disbursements from the California Department of Developmental Services (DDS)
+#' @name dds.dscr
+#' @docType data
+#' @references www.amstat.org/publications/jse/v22n1/mickel.pdf Taylor, Stanley A.,
+#'   and Amy E. Mickel. Simpson's paradox: A data set and discrimination case study
+#'   exercise. Journal of Statistics Education 22.1 (2014).
+#' @source Data contained in supplement B of Taylor and Mickel
+#' @description
+#' The dataset represents a sample of 1,000 DDS consumers
+#'     (out of a total population of approximately 250,000),
+#'     and includes information about age, gender, ethnicity, and the amount of
+#'     financial support per consumer provided by the DDS.The dataset is based
+#'     on recorded attributes of consumers,
+#'     but has been altered to maintain consumer privacy.
+#'
+#' @format A tibble with 1000 rows and 6 variables:
+#' \format{
+#' \describe{
+#'    \item{\code{id}}{Numeric, Unique identification code for each resident}
+#'    \item{\code{age.cohort}}{A factor variable, \code{0-5} years,
+#'      \code{6-12} years, \code{13-17} years, \code{18-21} years, \code{22-50} years,
+#'      and \code{51+} years}
+#'    \item{\code{age}}{Numberic, Age measured in years}
+#'    \item{\code{gender}}{A factor, Gender,  \code{Female} or \code{Male}.}
+#'    \item{\code{expenditures}}{Numeric, Amount of expenditures spent by the
+#'       State on an individual annually, measured in USD}
+#'   \item{\code{ethnicity}}{Factor, Ethnic group, recorded as
+#'       \code{American Indian},  \code{Asian}, \code{Black}, \code{Hispanic},
+#'       \code{Multi Race}, \code{Native Hawaiian}, \code{Other},
+#'       \code{White not Hispanic}}
+#'   }
+
+"dds.discr"
+
