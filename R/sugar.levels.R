363783fbb7e7b3c0237b3190763890e13c1487f6
OI-Biostat <38590928+dave-harrington@users.noreply.github.com>
2024-06-23 15:08:48 -0400

    Corrected error to units in sugar.levels

--- R/sugar.levels.R
+++ R/sugar.levels.R
@@ -1,8 +1,8 @@
-#' Simulated sugar (glucose) levels for 100 residents of a hypothetical neighborhood A
+#' Simulated fasting blood sugar levels for 100 residents of a hypothetical neighborhood A
 
-#' @Description  Simulated individual fasting blood sugar levels (mg/dL) drawn from 
+#' @description  Simulated individual fasting blood sugar levels (nmol/L) drawn from 
 #'     a normal distribution. Generally, normal fasting blood sugar level are 
-#'     between 70–100 mg/dL; levels in the range 100–125 mg/dL are considered pre-diabetes. 
+#'     between 3.0 - 5.6 nmol/L; levels in the range 5.6 - 6.9 nmol/L are considered pre-diabetes. 
 #'     These data and sugar.levels.B are used in the Unit 4 lab of  
 #'     Introductory Statistics for the Life and Biomedical Sciences.  See 
 #'     \url{https://github.com/OI-Biostat/oi_biostat_labs} for the full set of labs.
@@ -11,16 +11,16 @@
 #'
 #' @format A tibble with 100 rows and 1 variable
 #'\describe{
-#    \item{fasting.blood.sugar}{Numeric, simulated fasting blood sugar in mg/dL}
+#    \item{fasting.blood.sugar}{Numeric, simulated fasting blood sugar in nmol/L}
 #'    }
 #'
 "sugar.levels.A"
 
-#' Simulated sugar (glucose) levels for 100 residents of a hypothetical neighborhood B
+#' Simulated fasting blood sugar levels for 100 residents of a hypothetical neighborhood B
 
-#' @Description  Simulated individual fasting blood sugar levels (mg/dL) drawn from 
+#' @description  Simulated individual fasting blood sugar levels (nmol/L) drawn from 
 #'     a normal distribution. Generally, normal fasting blood sugar level are 
-#'     between 70–100 mg/dL; levels in the range 100–125 mg/dL are considered pre-diabetes. 
+#'     between 3.0 - 5.6 mg/dL; levels in the range 5.6 - 6.9 mg/dL are considered pre-diabetes. 
 #'     These data and sugar.levels.A are used in the Unit 4 lab of  
 #'     Introductory Statistics for the Life and Biomedical Sciences.  See 
 #'     \url{https://github.com/OI-Biostat/oi_biostat_labs} for the full set of labs.
@@ -29,7 +29,7 @@
 #'
 #' @format A tibble with 100 rows and 1 variable
 #'\describe{
-#    \item{fasting.blood.sugar}{Numeric, simulated fasting blood sugar in mg/dL}
+#    \item{fasting.blood.sugar}{Numeric, simulated fasting blood sugar in nmol/L}
 #'    }
 #'
 "sugar.levels.B"
